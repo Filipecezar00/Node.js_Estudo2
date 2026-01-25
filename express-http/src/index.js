c227002e9da5f0731e1389eb431453dd926916a0
Filipe Cezar <cezarfilipe01@gmail.com>
2026-01-25 19:06:54 -0300

    atualizando

--- express-http/src/index.js
+++ express-http/src/index.js
@@ -7,8 +7,21 @@ const clientes = [
     {id:4,nome:"João",telefone:"71 3211-4444"} 
 ];
 
+
+// Retorna todos os Clientes
 app.get("/users",(req,res)=>{
 return res.json(clientes)
 }); 
+
+//Buscar um unico Recurso 
+app.get("/users/:id",(req,res)=>{
+const user = clientes.filter(value=>value.id == req.params.id);
+res.json(user)
+})
+
+// Inserção de Dados
+app.post("/users",(req,res)=>{
+    console.log(req.body) 
+})
 
 app.listen(3000); 
