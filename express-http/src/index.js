918f22e12ef4e7faf29786d84bad8d58b089d0a6
Filipe Cezar <cezarfilipe01@gmail.com>
2026-01-26 16:09:10 -0300

    atualizando

--- express-http/src/index.js
+++ express-http/src/index.js
@@ -19,41 +19,53 @@ app.use(log)
 
 // Retorna todos os Clientes
 app.get("/users",(req,res)=>{
-return res.json(clientes)
+return res.json(clientes).status(200) 
 }); 
 
 //Buscar um unico Recurso 
 app.get("/users/:id",(req,res)=>{
-const user = clientes.filter(value=>value.id == req.params.id);
-res.json(user)
+const {id} = req.params; 
+const client = clientes.find(value=>value.id==id) 
+if(client==undefined){
+    res.status(400)
+}else{
+    res.status(200).json(client) 
+}
+// const user = clientes.filter(value=>value.id == req.params.id);
+// res.json(user)
 })
 
 // Inserção de Dados
-app.post("/users", log,(req,res)=>{
+app.post("/users",log,(req,res)=>{
     const cliente = req.body 
-    clientes.push(req.body)
-    res.json(cliente) 
+    clientes.push(cliente)
+    res.json(cliente).status(201) 
 
 })
 
 // Atualizar dados
 app.put("/users/:id",log,(req,res)=>{
-
     const id = req.params.id; 
     const nome = req.body.nome; 
-
-    let client = clientes.filter(value=>value.id == id); 
-
-    client[0].nome = nome  
-
-    res.json(client[0]);  
+    
+    let client = clientes.find(value=>value.id == id); 
+    if(client==undefined){
+        res.status(400)
+    }else{
+    client.nome = nome; 
+    res.json(client).status(200)   
+    }
 })
 
 app.delete("/users/:id",(req,res)=>{
-    const id = req.params.id; 
-    clientes=clientes.filter(value=>value.id != id);  
-    res.json(clientes);  
-
+    const {id} = req.params; 
+    const Index = clientes.findIndex(value=>value.id == id); 
+    if(Index == -1){
+        res.status(400).send() 
+    }else{
+        clientes.splice(Index,1) 
+        res.status(204)
+    }
 })
 
 app.listen(3000); 
